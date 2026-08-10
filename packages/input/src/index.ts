97051757e44b2acc4d4a04d7488a8bc900d187f7
Timur Dudhasch <155721920+dudhasch@users.noreply.github.com>
2026-08-10 12:14:00 +0200

    feat(input): S27 Eingabe-Abstraktion (package, index, actions)

---
+++ packages/input/src/index.ts
@@ -0,0 +1,65 @@
+export {
+  SEMANTIC_ACTIONS,
+  INPUT_CONTEXTS,
+  isSemanticAction,
+  isInputContext,
+  sortActions,
+  isNeutralSample,
+  type SemanticAction,
+  type InputContextId,
+  type InputSourceKind,
+  type ActionSample,
+  type ActionFrame,
+} from './actions.js';
+export {
+  AXIS_STUFEN,
+  AXIS_DEADZONE,
+  quantizeAxis,
+  dequantizeAxis,
+  axisFromDigital,
+} from './quantize.js';
+export {
+  defaultBindings,
+  serializeBindings,
+  parseBindings,
+  BINDINGS_SCHEMA_VERSION,
+  type BindingSet,
+  type BindingTable,
+  type ParseBindingsResult,
+} from './bindings.js';
+export {
+  KeyboardFeed,
+  GamepadFeed,
+  TouchFeed,
+  type InputSource,
+  type SourceSample,
+  type GamepadState,
+} from './sources.js';
+export { InputSampler } from './sampler.js';
+export {
+  ActionRecorder,
+  recordingDigest,
+  inputTicksDigest,
+  shiftRecording,
+  frameForTick,
+  ACTION_RECORDING_SCHEMA_VERSION,
+  type ActionRecording,
+  type RecordedActionFrame,
+} from './recording.js';
+export { toFieldInput, fieldInputPlan } from './field-adapter.js';
+export {
+  resolveTouchLayout,
+  hitTest,
+  sizeClassOf,
+  orientationOf,
+  layoutUnit,
+  DEFAULT_TOUCH_LAYOUT,
+  type TouchLayoutSpec,
+  type TouchControlSpec,
+  type ResolvedControl,
+  type Viewport,
+  type SafeArea,
+  type SizeClass,
+  type Orientation,
+  type Anchor,
+} from './layout.js';
